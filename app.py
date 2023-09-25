5ae4dc65487a6cc9937211870e28716d6a27ee46
Gokulsenthil <gokulsenthil899@gmail.com>
2023-09-26 01:05:11 +0530

    web app

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title = 'Excel plotter')
+st.title('Excel plotter📈')
+st.subheader('Feed me with your Excel file')
+
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('----')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
+    x_column = st.selectbox('Selecgitt X-axis Column:', df.columns)
+    y_column = st.selectbox('Select Y-axis Column:', df.columns)
+    fig = px.bar(df, x=x_column, y=y_column, template='plotly_dark')
+    st.plotly_chart(fig)
+    
+    
+    
+    
+    
