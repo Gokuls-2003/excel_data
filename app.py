25a27ea0ff292ad464b97b29e7066e395ca3d16c
Gokulsenthil <gokulsenthil899@gmail.com>
2023-09-26 12:13:55 +0530

    added requirements file

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title = 'Excel plotter')
-st.title('Excel plotter📈')
+st.set_page_config(page_title = 'CSV plotter')
+st.title('CSV plotter📈')
 st.subheader('Feed me with your Excel file')
 
-uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 if uploaded_file:
     st.markdown('----')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    df = pd.read_csv(uploaded_file)
     st.dataframe(df)
     x_column = st.selectbox('Selecgitt X-axis Column:', df.columns)
     y_column = st.selectbox('Select Y-axis Column:', df.columns)
